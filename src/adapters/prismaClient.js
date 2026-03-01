a3727fb10df392282e870a64429c1dc09896fe44
Emmanuel Avellaneda <emmanuelavellaneda4@gmail.com>
2026-03-01 02:04:37 -0500

    fix: corregir importacion de Prisma Client en microservicios
    - Cambiar de path join() a URL absoluta con new URL() para compatibilidad ESM en Windows
    - Los servicios ahora importan correctamente @prisma/client desde la raiz del proyecto
    - Solucion al error ERR_UNSUPPORTED_ESM_URL_SCHEME en ms-jobs y ms-scraping

--- src/adapters/prismaClient.js
+++ src/adapters/prismaClient.js
@@ -1,6 +1,11 @@
 import dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
+import { pathToFileURL } from "url";
 
 dotenv.config({ path: new URL("../../../../.env", import.meta.url) });
+
+// Importar PrismaClient desde la raíz del proyecto usando URL absoluta
+const prismaClientPath = new URL("../../../../node_modules/@prisma/client/index.js", import.meta.url);
+
+const { PrismaClient } = await import(prismaClientPath);
 
 export const prisma = new PrismaClient();
