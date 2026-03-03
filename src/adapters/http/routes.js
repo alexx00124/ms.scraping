408fea6b4ae10e374a0b65e31cf41dfb170837db
alexx00124 <pinedaalexx00@gmail.com>
2026-03-03 00:25:13 -0500

    update routes.js: aplicar ajustes del flujo actual

--- src/adapters/http/routes.js
+++ src/adapters/http/routes.js
@@ -1,13 +1,5 @@
 import express from "express";
 import { buildScrapingController } from "./scrapingController.js";
-
-export const buildRoutes = (scrapingService) => {
-	const router = express.Router();
-	const controller = buildScrapingController(scrapingService);
-
-	router.post("/scraping/job", controller.ingestOne);
-	router.post("/scraping/jobs", controller.ingestMany);
-
 import { buildScrapingSourceController } from "./scrapingSourceController.js";
 
 export const buildRoutes = (scrapingService, scrapingSourceRepository) => {
